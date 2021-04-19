6237ec94d888274d1e2fa2ba4168aebeff62c0de
alejandro-sin <alejandro.chem.io@gmail.com>
2021-04-19 10:31:11 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 
 
-from application import routes
+from app import routes
 
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from application import app
+from app import app
 from flask import render_template
 
 @app.route("/")
